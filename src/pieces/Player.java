62543aed691585d02179ccebe6272f43dde43a9c
Dylan Boland <dylan.boland@ucdconnect.ie>
2021-11-11 16:31:50 +0000

    Have added the basic fields as well as the get and set methods to the Player class

--- src/pieces/Player.java
+++ src/pieces/Player.java
@@ -1,5 +1,29 @@
 package pieces;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Player {
-
+	private String name;
+	private String colour;
+	private Map<String, Integer> resources = new HashMap<String, Integer>();
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getColour() {
+		return this.colour;
+	}
+	
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	
+	public Map<String, Integer> getResources() {
+		return this.resources;		
+	}
 }
