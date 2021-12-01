08d69a0cc0891d3fb9783b8fc2580633de04adda
Dylan Boland <dylan.boland@ucdconnect.ie>
2021-12-01 13:19:24 +0000

    Updated constructor calls inside the Board class - the name for the Marketplace and Stockpile objects are specified.

--- src/model/board/Board.java
+++ src/model/board/Board.java
@@ -2,7 +2,6 @@ package model.board;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import board.*;
 
 
 public class Board {
@@ -25,8 +24,8 @@ public class Board {
 		this.lairLocations = IntStream.rangeClosed(1, 37).boxed().collect(Collectors.toList());
 		this.shipLocations = IntStream.rangeClosed(1, 37).boxed().collect(Collectors.toList());
 		this.islands = new ArrayList<Islands>();
-		this.marketplace = new Marketplace();
-		this.stockpile = new Stockpile();
+		this.marketplace = new Marketplace("The marketplace");
+		this.stockpile = new Stockpile("The stockpile");
 	}
 	
 //	public void addIsland(Islands island) {
