5267a7c17d06f418ba295784a6840e16edb121e4
Dylan Boland <dylan.boland@ucdconnect.ie>
2021-12-02 17:34:23 +0000

    Creating the Controller class, which is stored within the controller package.

---
+++ src/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Controller {
+	private Model model;
+	private View view;
+	
+	public Controller(Model model, View view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+}
