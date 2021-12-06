e7b31b9b3acafb121a21f92d1e750d36f1769ea8
Dylan Boland <dylan.boland@ucdconnect.ie>
2021-12-06 12:19:30 +0000

    Updated the swap() method - it takes in the asset type in order to invoke the correct method: addLairAsset() or addShipAsset().

--- src/model/gameplay/Build.java
+++ src/model/gameplay/Build.java
@@ -1,21 +1,21 @@
 package model.gameplay;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
-import model.board.*;
+import model.board.Board;
+import model.board.Player;
 
 public class Build {
 	private Player player;
 	private Board board;
 	private String buildChoice;
-	private HashMap<String,Integer> playerResources;
-	private HashMap<String,Integer> lairCost = new HashMap<String,Integer>();
-	private HashMap<String,Integer> shipCost = new HashMap<String,Integer>();
+	private HashMap<String, Integer> playerResources;
+	private HashMap<String, Integer> lairCost = new HashMap<String, Integer>();
+	private HashMap<String, Integer> shipCost = new HashMap<String, Integer>();
 	private Scanner inputScanner;
-	
+
 	public Build(Player player, Scanner inputScanner) {
 		this.player = player;
 		this.board = Board.getInstance();
@@ -27,39 +27,42 @@ public class Build {
 		this.shipCost.put("Goats", 1);
 		this.shipCost.put("Wood", 1);
 	}
-	
+
 	public boolean checkResources() {
-		if(player.isAvailable("Wood", 1) && player.isAvailable("Goats", 1)) {
-			if(this.buildChoice.contains("Ship")) {
+		if (player.isAvailable("Wood", 1) && player.isAvailable("Goats", 1)) {
+			if (this.buildChoice.contains("Ship")) {
 				return true;
-			}
-			else if(player.isAvailable("Cutlass", 1) && player.isAvailable("Molasses", 1)){
+			} else if (player.isAvailable("Cutlass", 1) && player.isAvailable("Molasses", 1)) {
 				return true;
 			}
 		}
 		System.out.println("you do not have enough resources");
 		return false;
 	}
-	
+
 	public void buildLair() {
 		System.out.println("Where would you like to build a lair? (1-32)");
 		String lairLocation = inputScanner.nextLine();
 		this.buildChoice = "Lair";
-		//playerResources = player.getResources();
-		if(board.isLairAvailable(lairLocation) && checkResources()) {
-			swap(this.lairCost, lairLocation);
+		// playerResources = player.getResources();
+		if (board.isLairAvailable(lairLocation) && checkResources()) {
+			swap(this.lairCost, lairLocation, "Lair");
 			System.out.println("You now own lair " + lairLocation);
 		}
 	}
-	
-	private void swap(HashMap<String, Integer> costs, String asset) {
-		player.addAsset(asset);
-		for(Map.Entry cost: costs.entrySet() ) {
-			player.takeResource((String)cost.getKey(), (Integer)cost.getValue());
+
+	private void swap(HashMap<String, Integer> costs, String asset, String type) {
+		if (type == "Ship") {
+			player.addShipAsset(asset);
+		} else {
+			player.addLairAsset(asset);
+		}
+		for (Map.Entry cost : costs.entrySet()) {
+			player.takeResource((String) cost.getKey(), (Integer) cost.getValue());
 		}
 
 	}
-	
+
 	public void buildShip() {
 		this.buildChoice = "Ship";
 		System.out.println("Building Ship");
