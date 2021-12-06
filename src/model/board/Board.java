1edca31a84877003fff3807a9e6d3c3935cffcd4
Dylan Boland <dylan.boland@ucdconnect.ie>
2021-12-06 12:18:02 +0000

    getLairAssets() is now called in the for-each loop instead of the getAssets().

--- src/model/board/Board.java
+++ src/model/board/Board.java
@@ -1,71 +1,73 @@
 package model.board;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Board {
-	
+
 	private static Board gameBoard;
-	private List<Integer> lairLocations ;
-	private List<Islands> islands ;
-	private Marketplace marketplace ;
-	private Stockpile stockpile ;
+	private List<Integer> lairLocations;
+	private List<Islands> islands;
+	private Marketplace marketplace;
+	private Stockpile stockpile;
 	private PlayerList playerList;
-	
-    public static Board getInstance(){
-        if(gameBoard == null){
-            gameBoard = new Board();
-        }
-        return gameBoard;
-    }
-    
+
+	public static Board getInstance() {
+		if (gameBoard == null) {
+			gameBoard = new Board();
+		}
+		return gameBoard;
+	}
+
 	private Board() {
 		this.lairLocations = IntStream.rangeClosed(1, 32).boxed().collect(Collectors.toList());
 		this.islands = new ArrayList<Islands>();
 		this.marketplace = new Marketplace("The marketplace");
 		this.stockpile = new Stockpile("The stockpile");
-		this.playerList  = PlayerList.getInstance();
+		this.playerList = PlayerList.getInstance();
 	}
-	
+
 	public void setIslands(List<Islands> islands) {
 		this.islands = islands;
 	}
 
 	public boolean isLairAvailable(String location) {
-		for(Player player: playerList.getList()) {
-			if(player.getAssets().contains(location)) {
+		for (Player player : playerList.getList()) {
+			if (player.getLairAssets().contains(location)) {
 				System.out.println("Lair location is not available");
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	public void updateLairLocations(String location) {
 		this.lairLocations.remove(this.lairLocations.indexOf(location));
-	} 
+	}
 
 	public String getIslandInfo() {
 		String islandInfo = "";
-		for(Islands island: islands) {
+		for (Islands island : islands) {
 			islandInfo += island.toString() + "\n";
 		}
 		return islandInfo;
 	}
-	
+
 	public Marketplace getMarketplace() {
 		return this.marketplace;
 	}
-	
-	public Stockpile  getStockpile() {
+
+	public Stockpile getStockpile() {
 		return this.stockpile;
 	}
-	
-	public List<Islands> getIslands(){
+
+	public List<Islands> getIslands() {
 		return this.islands;
 	}
-	
-	public List<Integer> getLairList(){
+
+	public List<Integer> getLairList() {
 		return this.lairLocations;
 	}
 }
